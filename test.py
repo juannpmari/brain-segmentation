992001a8ed58b7ec976ea32da12a7f53bba60e33
Juan Mari <juannpmari@gmail.com>
2022-10-08 21:55:51 -0300

    test

--- test.py
+++ test.py
@@ -1,16 +1,34 @@
 from conventional_unet import ConvUnet
 from matplotlib import pyplot as plt
-from tensorflow import keras
+import tensorflow as tf
+#from tensorflow import keras
 
 mod=ConvUnet()
 X_train,y_train,X_valid,y_valid,X_test,y_test = mod.load_data() #(imgs,rows,cols)
+ 
+with tf.device('/cpu:0'):    
+    mod.ModelCompile()
+    n_train=300
+    mod.ModelFit(X_train[0:n_train,:,:],y_train[0:n_train,:,:],X_valid,y_valid) #No entran todas las imágenes en memoria
 
-mod.ModelCompile()
-mod.ModelFit(X_train[0:300,:,:],y_train[0:300,:,:],X_valid,y_valid) #No entran todas las imágenes en memoria
-
-predictions = mod.ModelPredict(X_test)
+n_test=3#len(X_test)
+predictions = mod.ModelPredict(X_test[0:n_test,:,:],"cpu")
 post_pred = mod.post_processing(predictions)
-mod.diceMetrics(y_test,post_pred,"full_model")
+mod.diceMetrics(y_test[0:n_test,:,:],post_pred,"boxplot_05")
 #mod.print_examples(X_test,post_pred,{0,10,15})
 
 
+f, (ax1,ax2) = plt.subplots(1, 2, sharey=True)
+ax1.imshow(X_test[0,:,:])
+ax2.imshow(post_pred[0,:,:])
+plt.show()
+
+f, (ax1,ax2) = plt.subplots(1, 2, sharey=True)
+ax1.imshow(X_test[1,:,:])
+ax2.imshow(post_pred[1,:,:])
+plt.show()
+
+f, (ax1,ax2) = plt.subplots(1, 2, sharey=True)
+ax1.imshow(X_test[2,:,:])
+ax2.imshow(post_pred[2,:,:])
+plt.show()
